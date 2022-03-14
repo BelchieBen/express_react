f3376df1c63526dab147e64130df42c6f71168b7
benjamin.belcher <benjamin.belcher@ideagen.com>
2022-03-14 13:07:21 +0000

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express'); 
+const path = require('path');
+
+const PORT = process.env.POST || 3001;
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+
+app.get("/api", (req,res)=> {
+    res.json({message: "Hello from server"});
+})
+
+app.get('*', (req,res)=> {
+    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+});
+
+app.listen(PORT, ()=> {
+    console.log('listening on port ${PORT}');
+})
+
